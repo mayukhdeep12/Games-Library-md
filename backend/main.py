b4fce0838e0be6ca6158bf9ae8e31ef018b14f5c
mayuk <mayukhdeep12@gmail.com>
2021-08-16 17:11:24 +0530

    Initial

---
+++ backend/main.py
@@ -0,0 +1,99 @@
+from flask import Flask, jsonify, request 
+from flask_cors import CORS
+import uuid
+
+app = Flask(__name__)
+
+app.config.from_object(__name__)
+
+CORS(app, resources={r"/*":{'origins':"*"}})
+# CORS(app, resources={r'/*':{'origins': 'http://localhost:8080',"allow_headers": "Access-Control-Allow-Origin"}})
+
+# hello world route
+@app.route('/', methods=['GET'])
+def greetings():
+    return("Hello, world!")
+
+@app.route('/shark', methods=['GET'])
+def shark():
+    return("Shark🦈!")
+
+
+GAMES = [
+
+    {   'id': uuid.uuid4().hex,
+        'title':'Rocket League',
+        'genre':'Football',
+        'played': True,
+    },
+    {   'id': uuid.uuid4().hex,
+        'title':'CSGO',
+        'genre':'Shooting',
+        'played': False,
+    },
+    {   'id': uuid.uuid4().hex,
+        'title':'Valorant',
+        'genre':'Shooting',
+        'played': True,
+    },
+    {  'id': uuid.uuid4().hex,
+        'title':'Planet Coaster',
+        'genre':'Entertainment',
+        'played': False,
+    },
+    {   'id': uuid.uuid4().hex,
+        'title':'GTA V',
+        'genre':'City',
+        'played': True,
+    }
+
+]
+
+# The GET and POST route handler
+@app.route('/games', methods=['GET', 'POST'])
+def all_games():
+    response_object = {'status':'success'}
+    if request.method == "POST":
+        post_data = request.get_json()
+        GAMES.append({
+            'id' : uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'genre': post_data.get('genre'),
+            'played': post_data.get('played')})
+        response_object['message'] =  'Game Added!'
+    else:
+        response_object['games'] = GAMES
+    return jsonify(response_object)
+
+
+#The PUT and DELETE route handler
+@app.route('/games/<game_id>', methods =['PUT', 'DELETE'])
+def single_game(game_id):
+    response_object = {'status':'success'}
+    if request.method == "PUT":
+        post_data = request.get_json()
+        remove_game(game_id)
+        GAMES.append({
+            'id' : uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'genre': post_data.get('genre'),
+            'played': post_data.get('played')
+        })
+        response_object['message'] =  'Game Updated!'
+    if request.method == "DELETE":
+        remove_game(game_id)
+        response_object['message'] = 'Game removed!'    
+    return jsonify(response_object)
+
+
+# Removing the game to update / delete
+def remove_game(game_id):
+    for game in GAMES:
+        if game['id'] == game_id:
+            GAMES.remove(game)
+            return True
+    return False
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
